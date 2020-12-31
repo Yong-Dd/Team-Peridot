d9074ca85ab9e1cec85dc6bf562fcff4fb627486
Yong-Dd <71385485+Yong-Dd@users.noreply.github.com>
2020-12-31 17:57:21 +0900

    2차 통합

--- app/src/main/java/com/peridot/o_der/client/PaymentPage.java
+++ app/src/main/java/com/peridot/o_der/client/PaymentPage.java
@@ -13,9 +13,9 @@ public class PaymentPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.payment_page2);
+        setContentView(R.layout.payment_page);
 
-        //리싸이클러뷰 adapter 설정
+        //리싸이클러뷰 LinearLayoutManager 설정
         RecyclerView recyclerView = findViewById(R.id.payment_recyclerview);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
@@ -24,8 +24,9 @@ public class PaymentPage extends AppCompatActivity {
         //임의 자료 추가
         adapter.addItem(new Payment("아메리카노","3000원"));
         adapter.addItem(new Payment("카페라떼","4500원"));
-        adapter.addItem(new Payment("당근 케익","5,500원"));
+        adapter.addItem(new Payment("당근 케익","5500원"));
 
+        //리싸이클러뷰 adapter 설정
         recyclerView.setAdapter(adapter);
 
         // X버튼 클릭 - 전 페이지로 이동
