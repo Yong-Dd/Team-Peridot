5b868b27b55f45e4b0ed5e5d10d576ed67f59933
Yong-Dd <71385485+Yong-Dd@users.noreply.github.com>
2021-01-04 09:55:07 +0900

    payment.java parcelable implements

--- app/src/main/java/com/peridot/o_der/client/Payment.java
+++ app/src/main/java/com/peridot/o_der/client/Payment.java
@@ -1,13 +1,35 @@
 package com.peridot.o_der.client;
 
-public class Payment {
-    String coffeeName;
-    String coffeePrice;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public Payment(String coffeeName, String coffeePrice) {
+import java.io.Serializable;
+
+public class Payment implements Parcelable {
+    String coffeeName;
+    int coffeePrice;
+
+    public Payment(String coffeeName, int coffeePrice) {
         this.coffeeName = coffeeName;
         this.coffeePrice = coffeePrice;
     }
+
+    protected Payment(Parcel in) {
+        coffeeName = in.readString();
+        coffeePrice = in.readInt();
+    }
+
+    public static final Creator<Payment> CREATOR = new Creator<Payment>() {
+        @Override
+        public Payment createFromParcel(Parcel in) {
+            return new Payment(in);
+        }
+
+        @Override
+        public Payment[] newArray(int size) {
+            return new Payment[size];
+        }
+    };
 
     public String getCoffeeName() {
         return coffeeName;
@@ -17,11 +39,22 @@ public class Payment {
         this.coffeeName = coffeeName;
     }
 
-    public String getCoffeePrice() {
+    public int getCoffeePrice() {
         return coffeePrice;
     }
 
-    public void setCoffeePrice(String coffeePrice) {
+    public void setCoffeePrice(int coffeePrice) {
         this.coffeePrice = coffeePrice;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(coffeeName);
+        dest.writeInt(coffeePrice);
     }
 }
